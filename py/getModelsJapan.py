f9008c055ffbc7aae6d2ef1fc88ce646f529aefe
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-01-11 10:40:25 +0800

    Update getModelsJapan.py

--- py/getModelsJapan.py
+++ py/getModelsJapan.py
@@ -11,7 +11,7 @@ option = {
     "skipExistingAssets": True
 }
 
-ba_api = "https://yostar-serverinfo.bluearchiveyostar.com/r73_50_yim5nra9oql3gg50vfye.json"
+ba_api = "https://yostar-serverinfo.bluearchiveyostar.com/r75_52_49ajrpwcziy395uuk0jq.json"
 
 ba_api2 = "https://prod-noticeindex.bluearchiveyostar.com/prod/index.json"
 
